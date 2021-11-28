2ee2c10aadeaa77dd3c541cf8603eed52f1697da
Matthew Hidalgo <matthewhidalgo@ip-10-0-0-173.us-east-2.compute.internal>
2021-11-28 13:21:29 -0700

    esbuild service function built

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(e.target.value);
   };
 
-  const handleClick = () => {
-    console.log(input);
+  const handleClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+    setCode(result.code);
   };
 
   return (
